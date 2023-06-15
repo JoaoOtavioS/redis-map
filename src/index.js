df9f0565f40ce7316c3dea59b036d5f19e45b777
Rafael <52458010+rafosto@users.noreply.github.com>
2023-06-15 22:47:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,193 +2,189 @@
 const redis = require("redis");
 
 class RedisAPI {
+  #redis;
+  #monitor;
 
-    #redis;
-    #pubsub;
-    #monitor;
+  /**
+   * Initialize the RedisMap class.
+   * @param {Object} options Options.
+   * @param {String} options.url Redis url.
+   * @param {Function} options.monitor Callback function. Args: (type = "state / error", message = string).
+   * @param {Object} options.redisOptions Redis options. (Default: {}).
+   */
+  constructor(options) {
+    const url = options?.url;
 
-    /**
-     * Initialize the RedisMap class.
-     * @param {Object} options Options.
-     * @param {String} options.url Redis url.
-     * @param {Function} options.monitor Callback function. Args: (type = "state / error", message = string).
-     * @param {Object} options.redisOptions Redis options. (Default: {}).
-     */
-    constructor(options) {
-
-        const url = options?.url;
-
-        if (!url || url.match(/redis:\/\//g) === null) {
-            throw new Error("[@joaootavios/redis-map] Invalid redis url!");
-        };
-
-        this.#redis = redis.createClient({ url, ...options?.redisOptions });
-        this.#pubsub = this.#redis.duplicate();
-        this.#monitor = options?.monitor;
-
-        this.#redis.on("connect", () => this.#log("state", "[@joaootavios/redis-map] Redis client connected!"));
-        this.#pubsub.on("connect", () => this.#log("state", "[@joaootavios/redis-map] Redis pubsub connected!"));
-
-        this.#redis.on("error", (err) => this.#log("error", `[@joaootavios/redis-map] Redis error: ${err}`));
-        this.#pubsub.on("error", (err) => this.#log("error", `[@joaootavios/redis-map] Redis pubsub error: ${err}`));
-
+    if (!url || url.match(/redis:\/\//g) === null) {
+      throw new Error("[@joaootavios/redis-map] Invalid redis url!");
     }
 
-    async connect() {
+    this.#redis = redis.createClient({ url, ...options?.redisOptions });
+    this.#monitor = options?.monitor;
 
-        if (this.#pubsub.isOpen && this.#redis.isOpen) return;
-        await Promise.all([this.#redis.connect(), this.#pubsub.connect()]);
+    this.#redis.on("connect", () =>
+      this.#log("state", "[@joaootavios/redis-map] Redis client connected!")
+    );
 
-        // Enable expired events for keyspace notifications in all maps.
-        this.#pubsub.configSet("notify-keyspace-events", "Ex");
+    this.#redis.on("error", (err) =>
+      this.#log("error", `[@joaootavios/redis-map] Redis error: ${err}`)
+    );
+  }
 
-        return {
-            redis: this.#redis,
-            pubsub: this.#pubsub,
-            disconnect: this.disconnect.bind(this)
-        }
-    }
+  async connect() {
+    if (this.#redis.connected) return;
 
-    async disconnect() {
-        if (!this.#pubsub.isOpen && !this.#redis.isOpen) return;
-        await Promise.all([
-            this.#redis.quit(),
-            this.#pubsub.quit()
-        ]);
-    }
+    await this.#redis.connect();
 
-    #log(type, message) {
-        if (this.#monitor) this.#monitor(type, message);
-    }
+    // Enable expired events for keyspace notifications in all maps.
+    this.#redis.config("SET", "notify-keyspace-events", "Ex");
 
+    return {
+      redis: this.#redis,
+      disconnect: this.disconnect.bind(this),
+    };
+  }
+
+  async disconnect() {
+    if (!this.#redis.connected) return;
+    await this.#redis.quit();
+  }
+
+  #log(type, message) {
+    if (this.#monitor) this.#monitor(type, message);
+  }
 }
 
 class RedisMap {
-    #redis;
-    #pubsub;
-    #options;
+  #redis;
+  #options;
 
-    /**
-     * Initialize the RedisMap class.
-     * @param {Object} options Options.
-     * @param {String} options.name Name of the map. (Default: redis-map).
-     * @param {Boolean} options.sync Sync data from redis on init. (Default: true).
-     * @param {Function} options.monitor Callback function. Args: (type = "state / info / error", message = string, name = name of the map).
-     */
-    constructor(options) {
+  /**
+   * Initialize the RedisMap class.
+   * @param {Object} options Options.
+   * @param {String} options.name Name of the map. (Default: redis-map).
+   * @param {Boolean} options.sync Sync data from redis on init. (Default: true).
+   * @param {Function} options.monitor Callback function. Args: (type = "state / info / error", message = string, name = name of the map).
+   */
+  constructor(options) {
+    this.name = options?.name || "redis-map";
+    this.#options = options;
+    this.data = {};
 
-        this.name = options?.name || "redis-map";
-        this.#options = options;
-        this.data = {};
+    this.#redis = options?.connections.redis;
 
-        this.#redis = options?.connections.redis;
-        this.#pubsub = options?.connections.pubsub;
-
-        if (!(this.#redis.isOpen || this.#pubsub.isOpen)) {
-            throw new Error("[@joaootavios/redis-map] Redis client is not connected!");
-        };
-
-        if (this.#options?.sync !== false) this.sync();
-        this.#subscribe();
+    if (!this.#redis.connected) {
+      throw new Error("[@joaootavios/redis-map] Redis client is not connected!");
     }
 
-    get(key) {
-        return this.data[key] || null;
+    if (this.#options?.sync !== false) this.sync();
+    this.#subscribe();
+  }
+
+  get(key) {
+    return this.data[key] || null;
+  }
+
+  has(key) {
+    return key in this.data;
+  }
+
+  entries() {
+    return Object.entries(this.data);
+  }
+
+  /**
+   * Set a value to the map in local cache and redis.
+   * @param {String} key
+   * @param {*} value
+   * @param {Number} expire Expire key in seconds.
+   */
+  async set(key, value, expire) {
+    this.data[key] = value;
+
+    const pipeline = this.#redis.multi();
+    pipeline.mset(this.name, JSON.stringify(this.data));
+    pipeline.publish(this.name, JSON.stringify({ a: 1, key, value }));
+
+    if (expire) {
+      pipeline.set(`rmap-${this.name}-ex=${key}`, 0, "EX", expire);
     }
 
-    has(key) {
-        return key in this.data;
-    }
+    await pipeline.exec();
+  }
 
-    entries() {
-        return Object.entries(this.data);
-    }
+  /**
+   * Delete a value from the map in local cache and redis.
+   * @param {String} key
+   */
+  async delete(key) {
+    delete this.data[key];
 
-    /**
-     * Set a value to the map in local cache and redis.
-     * @param {String} key
-     * @param {*} value
-     * @param {Number} expire Expire key in seconds.
-     */
-    async set(key, value, expire) {
-        this.data[key] = value;
-        await Promise.all([
-            this.#redis.set(this.name, JSON.stringify(this.data)),
-            this.#publish({ a: 1, key, value })
-        ]);
-        if (expire) this.#redis.set(`rmap-${this.name}-ex=${key}`, 0, { EX: expire });
-    }
+    const pipeline = this.#redis.multi();
+    pipeline.mset(this.name, JSON.stringify(this.data));
+    pipeline.publish(this.name, JSON.stringify({ a: 2, key }));
 
-    /**
-     * Delete a value from the map in local cache and redis.
-     * @param {String} key
-     */
-    async delete(key) {
+    await pipeline.exec();
+  }
+
+  /**
+   * WARNING: This method will clear all data from the map in local cache and redis.
+   */
+  async clear() {
+    this.data = {};
+
+    const pipeline = this.#redis.multi();
+    pipeline.del(this.name);
+    pipeline.publish(this.name, JSON.stringify({ a: 3 }));
+
+    await pipeline.exec();
+  }
+
+  async sync() {
+    const data = await this.#redis.get(this.name).catch(() => null);
+
+    if (data) {
+      this.data = JSON.parse(data);
+      this.#log("info", `[${this.name}] Redis data synced!`);
+    }
+  }
+
+  #log(type, message) {
+    if (this.#options?.monitor) this.#options.monitor(type, message, this.name);
+  }
+
+  #subscribe() {
+    this.#redis.psubscribe("__keyevent@0__:expired");
+
+    this.#redis.on("pmessage", (pattern, channel, message) => {
+      if (channel === "__keyevent@0__:expired" && message.startsWith(`rmap-${this.name}-ex`)) {
+        const key = message.split("=")[1];
         delete this.data[key];
-        await Promise.all([
-            this.#redis.set(this.name, JSON.stringify(this.data)),
-            this.#publish({ a: 2, key })
-        ])
-    }
+      }
+    });
 
-    /**
-     * WARNING: This method will clear all data from the map in local cache and redis.
-     */
-    async clear() {
-        await Promise.all([
-            this.#redis.del(this.name),
-            this.#publish({ a: 3 })
-        ])
-    }
+    this.#redis.subscribe(this.name);
 
-    async sync() {
-        const data = await this.#redis.get(this.name).catch(() => null);
+    this.#redis.on("message", (channel, message) => {
+      const { a: action, key, value } = JSON.parse(message);
+      this.#log("info", { action, key, value });
 
-        if (data) {
-            this.data = JSON.parse(data);
-            this.#log("info", `[${this.name}] Redis data synced!`);
-        }
-    }
-
-    #log(type, message) {
-        if (this.#options?.monitor) this.#options.monitor(type, message, this.name);
-    }
-
-    async #publish(...data) {
-        const message = JSON.stringify(...data);
-        await this.#redis.publish(this.name, message);
-    }
-
-    #subscribe() {
-
-        this.#pubsub.pSubscribe("__keyevent@0__:expired", (data) => {
-            if (data.startsWith(`rmap-${this.name}-ex`)) {
-                delete this.data[data.split("=")[1]];
-            }
-        });
-
-        this.#pubsub.subscribe(this.name, (message) => {
-            const { a: action, key, value } = JSON.parse(message);
-            this.#log("info", { action, key, value });
-
-            switch (action) {
-                case 1:
-                    this.data[key] = value;
-                    break;
-                case 2:
-                    delete this.data[key];
-                    break;
-                case 3:
-                    this.data = {};
-                    break;
-            }
-
-        });
-    }
-
+      switch (action) {
+        case 1:
+          this.data[key] = value;
+          break;
+        case 2:
+          delete this.data[key];
+          break;
+        case 3:
+          this.data = {};
+          break;
+      }
+    });
+  }
 }
 
 module.exports = {
-    RedisAPI,
-    RedisMap
+  RedisAPI,
+  RedisMap,
 };
